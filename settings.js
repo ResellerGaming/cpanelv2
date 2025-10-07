5f6b1666c97aeda43296a37ea706316cd34d3590
Reseller Gaming Programer <resellergaming.official@gmail.com>
2025-10-07 17:08:42 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+// settings.js
+module.exports = {
+  githubToken: "ghp_YOUR_GITHUB_TOKEN_HERE", // token github 
+  repo: "your-username/nebula-signup", // ganti dengan username dan repo kamu
+  branch: "main" // jangan di otak-atik 
+};
