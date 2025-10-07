66bf90aca6e2b87e1d5011538c85b9829a188716
Reseller Gaming Programer <resellergaming.official@gmail.com>
2025-10-07 17:19:49 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 // settings.js
 module.exports = {
-  githubToken: "ghp_YOUR_GITHUB_TOKEN_HERE", // token github 
-  repo: "your-username/nebula-signup", // ganti dengan username dan repo kamu
+  githubToken: "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", // token github 
+  repo: "ResellerGaming/cpanelv2", // ganti dengan username dan repo kamu
   branch: "main" // jangan di otak-atik 
 };
